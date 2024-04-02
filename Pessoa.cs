56a9de525d1d83fcbae36276690cf86f35a65c95
Clayton1224 <claytonrichard153@gmail.com>
2024-04-02 16:36:43 -0400

    adicionado exemplo de classe Pessoa

---
+++ Pessoa.cs
@@ -0,0 +1,24 @@
+public class Pessoa {  
+    private string nome:
+private int idade:
+private string cpf:
+
+public Pessoa(string,nome, int idade , string cpf) {  
+      }
+      nome = nome;
+      idade = idade;.
+      CPF = cpf;
+      { public string nome {  
+        get{ return nome;} 
+        set{ nome =  value;}
+      }
+      public int idade{ 
+        get{ return nome;}
+        set{ idade = value;}
+      }
+      public string CPF{ 
+        get { return idade;}
+        set{ cpf = value;}
+      }
+}
+
